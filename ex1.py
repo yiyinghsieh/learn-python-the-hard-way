7ff4fe7546dbcafdd5f691c0b398184766766576
Yiying Hsieh <yiyinghsieh@Yiyingde-MacBook-Air.local>
2018-11-18 13:03:30 +0100

    Add ex1.py

---
+++ ex1.py
@@ -0,0 +1,9 @@
+# ex1.py
+
+print "Hello World!"
+print "Hello Again"
+print "I like to typing this."
+print "This is fun."
+print 'Yay! Printing.'
+print "I'd much rather you 'not'."
+print 'I "said" do not touch this.'
